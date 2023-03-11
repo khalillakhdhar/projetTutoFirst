c68d8711562b4a59974f3ffba4c516c7accb6dfd
khalil <khalillakhdharatc@gmail.com>
2023-03-11 11:13:01 +0100

    modéle

--- src/main/java/com/repo/entities/Tag.java
+++ src/main/java/com/repo/entities/Tag.java
@@ -1,6 +1,18 @@
 package com.repo.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +21,22 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name ="tags")
 public class Tag {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private long id;
+private String name;
+
+@ManyToMany(fetch = FetchType.LAZY,
+cascade = {
+		CascadeType.PERSIST,
+		CascadeType.MERGE
+},mappedBy = "tags"
+		)
+@JsonIgnore
+private Set<Tutorial> tutorials=new HashSet<>();
+
+
 
 }
--- src/main/java/com/repo/entities/Tutorial.java
+++ src/main/java/com/repo/entities/Tutorial.java
@@ -1,10 +1,20 @@
 package com.repo.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,6 +26,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "tutorials")
+@JsonIgnoreProperties({"hibernateLazyInitializer"})
 public class Tutorial {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,8 +37,25 @@ private String title;
 private String description;
 
 private boolean published;
-	
-	
-	
-	
+
+@ManyToMany(fetch = FetchType.LAZY,
+cascade = {
+		CascadeType.PERSIST,
+		CascadeType.MERGE
+}
+		)
+@JoinTable(name = "tutorial_tags",
+joinColumns = {
+		@JoinColumn(name="tutorial_id")},
+inverseJoinColumns = {@JoinColumn(name="tag_id")})
+private Set<Tag> tags=new HashSet<>();
+public void addTag(Tag tag)
+{
+this.tags.add(tag);
+tag.getTutorials().add(this);
+}
+
+
+
+
 }
